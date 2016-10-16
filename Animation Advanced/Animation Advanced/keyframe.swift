1d2b2be48740f1e79994c3318bfb15ebed2ba81e
kevin <kevinchou.c@gmail.com>
2016-10-17 01:32:52 +0800

    Update comment

--- Animation Advanced/Animation Advanced/keyframe.swift
+++ Animation Advanced/Animation Advanced/keyframe.swift
@@ -11,7 +11,11 @@ import UIKit
 extension DetailViewController {
     func keyFrame() {
         imageView.image = UIImage(named: "leaf")
+        
+        // clipsToBounds使得图片显示不超过imageView的bounds
         imageView.clipsToBounds = true
+        
+        // ScaleAspectFill避免图片在缩放的时候变形
         imageView.contentMode = UIViewContentMode.scaleAspectFill
         
         view.window!.addSubview(imageView)
@@ -20,20 +24,25 @@ extension DetailViewController {
     }
     
     func startKeyFrameAnimation() {
+        // 定义一个总时间为2秒的动画
         UIView.animateKeyframes(withDuration: 2, delay: 0, options: UIViewKeyframeAnimationOptions.calculationModeCubic, animations: {
             
+            // 在动画进度为0的时候开始，耗时0.5秒
             UIView.addKeyframe(withRelativeStartTime: 0.0, relativeDuration: 0.5) {
                 self.imageView.frame = CGRect(x: 0, y: 48, width: self.view.frame.width, height: 0)
             }
             
+            // 在动画进度为0.25的时候开始，耗时0.5秒
             UIView.addKeyframe(withRelativeStartTime: 0.25, relativeDuration: 0.5) {
                 self.imageView.frame = CGRect(x: 0, y: 32, width: self.view.frame.width, height: 260 + self.view.frame.height/4.0)
             }
             
+            // 在动画进度为0.5的时候开始，耗时0.5秒
             UIView.addKeyframe(withRelativeStartTime: 0.5, relativeDuration: 0.5) {
                 self.imageView.frame = CGRect(x: 0, y: 16, width: self.view.frame.width, height: 260 + self.view.frame.height/2.0)
             }
             
+            // 在动画进度为0.75的时候开始，耗时0.5秒
             UIView.addKeyframe(withRelativeStartTime: 0.75, relativeDuration: 0.5) {
                 self.imageView.frame = CGRect(x: 0, y: 0, width: self.view.frame.width, height: self.view.frame.height)
             }
--- Animation Advanced/Animation Advanced/maskAnimation.swift
+++ Animation Advanced/Animation Advanced/maskAnimation.swift
@@ -32,19 +32,36 @@ extension DetailViewController {
         let screenWidth = view.frame.width
         let screenHeight = view.frame.height
         
-        //
+        // 针对bounds的变形动画
         let transformAnim            = CAKeyframeAnimation(keyPath:"bounds")
+        
+        // values中定义了各个关键帧真对属性所需要修改的目标数值
         transformAnim.values         = [NSValue(cgRect:CGRect(x: 0, y: 0, width: 64, height: 64)) ,
                                         NSValue(cgRect:CGRect(x: 0, y: 0, width: 128, height: 128)) ,
                                         NSValue(cgRect:CGRect(x: 0, y: 0, width: 256, height: 256)) ,
                                         NSValue(cgRect:CGRect(x: 0, y: 0, width: view.frame.height*2 , height: view.frame.height*2)) ]
+        
+        // keyTimes和values是一一对应的关系，定义了每个关键帧
+        // 执行时相对于总时间的位置
         transformAnim.keyTimes       = [0, 0.349, 0.618, 1]
+        
+        // duration为总时间
         transformAnim.duration       = 1
+        
+        // removedOnCompletion定义了动画完成时是否从Layer上删除
         transformAnim.isRemovedOnCompletion = false
+        
+        // fillMode定义了动画完成时，对于Layer的属性修改是否保留
         transformAnim.fillMode = kCAFillModeForwards
+        
+        // 我们这里将removedOnCompletion定义为false，fillMode
+        // 定义为kCAFillModeForwards则可以使得动画结束时，保留
+        // Layer的结束状态，你可以通过注释到这两句，看看其默认效果
+        
         self.maskView.layer.add(transformAnim, forKey: "bounds")
         
-        //
+        // 定义位移动画，因为Core Animation不能操作frame，所以
+        // 需要拆分成position和bounds两个动画
         
         let positionAnim            = CAKeyframeAnimation(keyPath:"position")
         positionAnim.values         = [NSValue(cgPoint:CGPoint(x: screenWidth/8.0, y: screenHeight/8.0)) ,
@@ -57,7 +74,7 @@ extension DetailViewController {
         positionAnim.fillMode = kCAFillModeForwards
         self.maskView.layer.add(positionAnim, forKey: "position")
         
-        //
+        // 定义修改圆角的动画
         
         let radiusAnim            = CAKeyframeAnimation(keyPath:"cornerRadius")
         radiusAnim.values         = [32,64,128,view.frame.height]
